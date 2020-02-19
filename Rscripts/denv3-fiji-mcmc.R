ce7b718a75d4026d04dadd2c5546e53dca352f05
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2020-02-19 19:11:31 +0000

    DENV3 model fitting code added

---
+++ Rscripts/denv3-fiji-mcmc.R
@@ -0,0 +1,302 @@
+# - - - - - - - - - - - - - - - - - - - - - - - 
+# MCMC analysis of Zika transmission in Fiji  
+# Author: Alasdair Henderson
+# github.com/
+# - - - - - - - - - - - - - - - - - - - - - - - 
+library(here)
+preamblecode <- "preamble_denv3.R"
+source(here::here("Rscripts", preamblecode))
+
+r_functions <- list.files(here::here("Rfunctions/"), pattern="*.R")
+sapply(here::here("Rfunctions", r_functions), source, .GlobalEnv)
+
+# load data and setup with fixed DENV intro date Oct 27th 2013
+prior_vectorcontrol <- function(x){dunif(x,0,1)}
+denv_data <- load.data.multistart(Virus = "DEN3", startdate = start.output.date, serology.file.name = serology.excel, init.values.file.name = init.conditions.excel, add.nulls = 0) #virusTab[iiH], dataTab[iiH])
+  list2env(denv_data,globalenv())
+denv.timeseries <- denv_data$y.vals
+denv.dates <- denv_data$date.vals
+load(file=here::here("data/theta_denv.RData"))
+
+# Set up results objects, initial values and covariance matrices ----------
+setup <- results_set_up(iiH, "parameters_est_denv3")
+  list2env(setup, globalenv())
+theta_initAlltab[1,,]
+thetaAlltab[1,,]
+
+# Set up Priors  ----------------------------------------------------------
+source(here::here("Rscripts/prior_distributions.R"))
+
+priorIntro <- function(x){dunif(x, min=0, max=307)} ## DENV import mid point is 2013-11-07 (first case reported) at latest
+priorInitInf <- function(x){dunif(x, min=0, max=1000)} ## DENV import max is 1000 in one day
+  
+# load climate data -------------------------------------------------------
+weather.data <- read.csv(here::here("data/suva-temp.csv"), stringsAsFactors = T) # Load DLI dengue data
+weather.data$lsd <- as.Date(weather.data$lsd)
+
+first.temp.date <- min(as.Date(date.vals))
+data.series.max <- weather.data$max_air_temp[as.Date(weather.data$lsd)>=first.temp.date & as.Date(weather.data$lsd)<=max(as.Date(date.vals))]
+data.series.min <- weather.data$min_air_temp[as.Date(weather.data$lsd)>=first.temp.date & as.Date(weather.data$lsd)<=max(as.Date(date.vals))]
+data.series <- ((data.series.max-data.series.min)/2)+data.series.min
+date.series <- weather.data$lsd[as.Date(weather.data$lsd)>=first.temp.date & as.Date(weather.data$lsd)<=max(as.Date(date.vals))]
+plot(date.series, data.series, type='l')
+
+betas <- weather.fit(data=data.series, iter=min(MCMC.runs, 1e5), tuning=10)
+  save(betas,file=paste("posterior_output_betas/outputBetas.RData",sep=""))
+  load(file=paste("posterior_output_betas/outputBetas.RData",sep=""))
+
+  plot(betas$lik[2,],betas$mid,type='p')
+  max(betas$lik[2,],na.rm=T)
+  
+# Find seasonality parameters ---------------------------------------------
+mordecai_tempR0 <- read.csv(here::here("data/outputTemp_R0.csv"))
+btstrp <- 4000
+sin_sum <- NULL
+picks <- 1:length(betas$amp)
+b <- mean(data.series, na.rm = T)
+for(ii in 1:btstrp){
+  b_ii <- sample(picks,1)
+  sincurve <- b*seasonal_f(time = time.vals, date0 = 0, amp = betas$amp[b_ii], mid = betas$mid[b_ii])
+  sin_sum <- rbind(sin_sum, sincurve)
+}
+c.nume<-function(x){
+  bp1=c(median(x),quantile(x,0.25),quantile(x,0.75))
+  as.numeric(bp1)}
+sinwave_summ <- apply(sin_sum,2,c.nume) 
+plot(date.series, data.series, col="gray75", type='l')
+lines(date.vals, sinwave_summ[1,], type='l')
+peakBeta <- max(sinwave_summ[1,])
+lowBeta <- min(sinwave_summ[1,])
+peakBetaDate <- date.vals[sinwave_summ[1,]==peakBeta]
+lowBetaDate <- date.vals[sinwave_summ[1,]==lowBeta]
+peakBetaTemp <- round(peakBeta,1) #round(sum(weather.data[weather.data$lsd==peakBetaDate,3:4])/2,1)
+lowBetaTemp <- round(lowBeta,1) #round(sum(weather.data[weather.data$lsd==lowBetaDate,3:4])/2,1)
+## find in Mordecai data
+(peakR0 <- mordecai_tempR0[round(mordecai_tempR0$aegy.temps.DTR8,1)==peakBetaTemp,2])
+(lowR0 <- mordecai_tempR0[round(mordecai_tempR0$aegy.temps.DTR8,1)==lowBetaTemp,2])
+rel_lowR0 <- lowR0/peakR0 
+#print(rel_lowR0)
+mordecai_amp <- (1-rel_lowR0)/(1+rel_lowR0)
+print(c("mordecai_amp",mordecai_amp))
+
+# Check seasonality fit worked --------------------------------------------
+beta=mean(data.series, na.rm = T)
+(amp=median(betas$amp))
+(amp=mordecai_amp)
+(mid=median(betas$mid)) #(mid=0.00027)
+j=1; sincurve=NULL; date0=0
+for(i in 1:length(date.series)){
+  sincurve[j] <- beta*(1 + amp * sin(((i/365.25) - mid)*2*pi))
+  j=j+1
+}
+plot(date.series, data.series, type='l', ylim=c(min(min(data.series, na.rm=T), min(sincurve)), max(max(data.series, na.rm=T), max(sincurve))))
+lines(date.series, sincurve, col=2, type='l',yaxt="n")
+max_curve <- date.series[sincurve==max(sincurve)]
+print(paste0("max curve = ", max_curve))
+if(months(max_curve)!="February"){
+  error("Peak month of seeasonal wave is not in Feb. Check why")
+}
+
+burnin=0.2
+if(seasonal.transmission == T){
+  thetaAlltab[1,iiH,'beta_v_amp'] <- mordecai_amp
+  thetaAlltab[1,iiH,'beta_v_mid'] <- median(betas$mid[round(burnin*length(betas$mid)):length(betas$mid)])
+}else{
+  thetaAlltab[1,iiH,'beta_v_amp'] <- 0
+}  
+# set weakly informative priors for main model fit 
+priorBeta_amp<-function(x){dunif(x,min=0, max=1)} 
+priorBeta_mid<-function(x){dunif(x,min=0, max=1)} 
+
+# Print starting conditions for model run --------------------------
+print("Theta initial starting conditions")
+thetaAlltab[1,,]
+diag(cov_matrix_thetaAll)
+
+# Run MCMC loop and save results ------------------------------------------
+(st.time <- Sys.time())
+
+m = 1; iiM=1
+foreach(iiM=multichain) %dopar% {  # Loop over regions with parallel MCMC chains
+adapt_size_start=round(0.1*MCMC.runs)
+  for (m in 1:MCMC.runs){
+    # Scale COV matrices for resampling using error term epsilon
+    if(m==1){
+      epsilon0 = 0.0001
+      cov_matrix_theta=epsilon0*cov_matrix_theta0
+      cov_matrix_thetaA=epsilon0*cov_matrix_thetaAll
+      cov_matrix_theta_init=epsilon0*cov_matrix_theta_initAll
+      # set current values for vectors to be updated in MH algroithm 
+      thetatab_current = thetatab[m,]
+      thetaAlltab_current = thetaAlltab[m,,]
+      theta_initAlltab_current = theta_initAlltab[m,,]
+      c_trace_tab_current = c_trace_tab[m,,]
+      cd_trace_tab_current = cd_trace_tab[m,,]
+      s_trace_tab_current = s_trace_tab[m,,]
+      r_trace_tab_current = r_trace_tab[m,,]
+      x_trace_tab_current =  x_trace_tab[m,,]
+      sim_liktab_current = sim_liktab[m]
+      prior_current = prior[m]
+      covmat.empirical <- cov_matrix_thetaA
+      theta.mean <- thetaAlltab_current[1,]
+      # initialise counter for storing results (m/thining parameter)
+      j=1
+    }else{
+      scaling.multiplier <- exp((1-1e-7)^(m-adapt_size_start)*(accept_rate-0.234))
+      epsilon0 <- epsilon0 * scaling.multiplier
+      epsilon0 <- min(epsilon0, 0.5)
+      epsilon0 <- max(epsilon0, 1e-10)
+      cov_matrix_theta=epsilon0*cov_matrix_theta0
+      cov_matrix_thetaA=epsilon0*cov_matrix_thetaAll
+      cov_matrix_theta_init=epsilon0*cov_matrix_theta_initAll
+    }
+    
+    ## Resample global theta every 2nd step
+    if(m %% 2==0){
+      output_theta <- SampleTheta(global=1,thetatab_current,theta_initAlltab_current[iiH,],cov_matrix_theta,cov_matrix_theta_init)
+      theta_star <- output_theta$thetaS
+    }else{
+      theta_star <- thetatab_current
+    }
+    
+    ## Resample local parameters every step
+    prior.star=1
+    prior.current=1
+    sim_marg_lik_star=0
+    thetaAllstar=0*thetaAlltab_current
+    theta_initAllstar=0*theta_initAlltab_current
+    cTraceStar=0*c_trace_tab_current
+    cdTraceStar=0*cd_trace_tab_current
+    sTraceStar=0*s_trace_tab_current
+    rTraceStar=0*r_trace_tab_current
+    xTraceStar=0*x_trace_tab_current
+    for(kk in 2){ ## settng kk==2 means running the model for DENV3 not ZIKV
+      iiH <- kk
+      data <- load.data.multistart(Virus = "DEN3", startdate = start.output.date, serology.file.name = serology.excel, init.values.file.name = init.conditions.excel, add.nulls = 0) #virusTab[iiH], dataTab[iiH])
+        list2env(data,globalenv())
+        
+        if(m==1){ # Don't resample on 1st step - check the zeroes!
+          output_H <- SampleTheta(global=0,thetaAlltab_current[iiH,],theta_initAlltab_current[iiH,],0*cov_matrix_thetaA,0*cov_matrix_theta_init)
+        }else{
+          output_H <- SampleTheta(global=0,thetaAlltab_current[iiH,],theta_initAlltab_current[iiH,],cov_matrix_thetaA,cov_matrix_theta_init)
+        } 
+        thetaA_star <- output_H$thetaS
+        theta_init_star <- output_H$theta_initS
+        
+        # Run model simulation
+        output1 <- Deterministic_modelR_final_DENVimmmunity(theta=c(theta_star,thetaA_star,theta_denv), theta_init_star, locationI=locationtab[iiH], seroposdates=seroposdates, include.count=include.count)
+        (sim_marg_lik_star <- sim_marg_lik_star + output1$lik)
+        
+        #Store vales
+        thetaAllstar[iiH,] <- thetaA_star
+        theta_initAllstar[iiH,] <- theta_init_star
+        
+        # store results between prespecified dates
+        start.of.output1 <- 1
+        length.of.output1 <- length(output1$C_trace) - max(0,length(output1$C_trace) - length(cTraceStar[iiH,]))
+        extra.vals <- max(0,length(cTraceStar[iiH,]) - length(output1$C_trace))
+        extra.zero <- rep(0,extra.vals)
+        cTraceStar[iiH,]=c(extra.zero,output1$C_trace[(start.of.output1:length.of.output1)])
+        cdTraceStar[iiH,]=c(extra.zero,output1$CD_trace[(start.of.output1:length.of.output1)])
+        sTraceStar[iiH,]=c(extra.zero,output1$S_trace[(start.of.output1:length.of.output1)])
+        rTraceStar[iiH,]=c(extra.zero,output1$R_trace[(start.of.output1:length.of.output1)])
+        xTraceStar[iiH,]=c(extra.zero,output1$X_trace[(start.of.output1:length.of.output1)])
+        
+        # Calculate prior density for current and proposed theta set
+        prior.theta <- ComputePrior(iiH, c(thetatab_current,thetaAlltab_current[iiH,]), c(theta_star,thetaA_star), covartheta = cov_matrix_thetaA)
+        prior.star <- (prior.theta$prior.star*prior.star)
+        prior.current <- (prior.theta$prior*prior.current)
+    } # end loop over regions
+    
+    # Calculate probability function - MH algorithm
+    estimated_params <- colnames(cov_matrix_thetaA)[diag(cov_matrix_thetaA)!=0]
+    q_theta_given_theta_star <- sum(log(thetaAllstar[2, estimated_params]))
+    q_theta_star_given_theta <- sum(log(thetaAlltab_current[2, estimated_params]))
+    
+    val <- (prior.star/prior.current)*exp((sim_marg_lik_star-sim_liktab_current) +  (q_theta_given_theta_star - q_theta_star_given_theta)) 
+    
+    if(is.na(val)){
+      output_prob=0}else if(is.nan(val)){
+        output_prob=0}else if(is.null(val)){
+          output_prob=0}else if(length(val)==0){
+            output_prob=0}else{
+              output_prob = min(val, 1)}
+    
+    # Update parameter values every k step
+    MH_random_unif <- runif(1,0,1)
+    
+    if(m %% thinning.parameter == 0){
+      if(MH_random_unif < output_prob){
+        thetatab[j+1,] <- theta_star
+        thetaAlltab[j+1,,] <- thetaAllstar
+        theta_initAlltab[j+1,,] <- theta_initAllstar
+        c_trace_tab[j+1,,] <- cTraceStar
+        cd_trace_tab[j+1,,] <- cdTraceStar
+        s_trace_tab[j+1,,] <- sTraceStar
+        r_trace_tab[j+1,,] <- rTraceStar
+        x_trace_tab[j+1,,] <- xTraceStar
+        sim_liktab[j+1] <- sim_marg_lik_star
+        accepttab[j] <- 1
+        prior[j+1] <- prior.star
+      }else{
+        thetatab[j+1,] = thetatab[j,]
+        thetaAlltab[j+1,,] = thetaAlltab[j,,]
+        theta_initAlltab[j+1,,] = theta_initAlltab[j,,]
+        c_trace_tab[j+1,,]=c_trace_tab[j,,]
+        cd_trace_tab[j+1,,]=cd_trace_tab[j,,]
+        s_trace_tab[j+1,,]=s_trace_tab[j,,]
+        r_trace_tab[j+1,,]=r_trace_tab[j,,]
+        x_trace_tab[j+1,,]=x_trace_tab[j,,]
+        sim_liktab[j+1] = sim_liktab[j]
+        accepttab[j]=0
+        prior[j+1] = prior[j] 
+      }
+      accept_rate <- sum(accepttab[1:j])/j
+      j <- j+1
+    }
+    
+    # Update current values of parameter if{MH_algorithm_val > runif(1,0,1)}
+    if(MH_random_unif < output_prob){
+      thetatab_current = theta_star
+      thetaAlltab_current = thetaAllstar
+      theta_initAlltab_current = theta_initAllstar
+      c_trace_tab_current = cTraceStar
+      cd_trace_tab_current = cdTraceStar
+      s_trace_tab_current = sTraceStar
+      r_trace_tab_current = rTraceStar
+      x_trace_tab_current = xTraceStar
+      sim_liktab_current = sim_marg_lik_star
+      prior_current = prior.star
+    }
+    
+    if(m<20){
+      accept_rate <- 0.234
+    }
+    
+    if(m %% min(MCMC.runs,500)==0){
+      print(c(iiM,"m"=m,  
+              "acc"=signif(accept_rate,3), 
+              "lik"=signif(sim_liktab_current,3),
+              signif(thetaAlltab_current[2,'beta_h'],3),
+              signif(thetaAlltab_current[2,'beta_v'],3),
+              thetaAlltab_current[2,'beta_base'],
+              thetaAlltab_current[2,'intro_base']),
+              digits = 2)
+      save(sim_liktab,
+           prior,
+           accepttab,
+           c_trace_tab,
+           cd_trace_tab,
+           s_trace_tab,
+           r_trace_tab,
+           x_trace_tab,
+           thetatab,
+           thetaAlltab,
+           theta_initAlltab,
+           file=paste("posterior_denv2014fit/outputR_",iiM,"_",run.name,".RData",sep=""))
+    }
+  } # End MCMC loop
+}
+
+endtime <- Sys.time()
+(time.take = endtime-st.time)
---
+++ Rscripts/preamble_denv3.R
@@ -0,0 +1,193 @@
+# - - - - - - - - - - - - - - - - - - - - - - - 
+# Transmission modelling preamble
+# Author: Alasdair Henderson
+# github.com/a-henderson91/fiji-zikv-model
+# - - - - - - - - - - - - - - - - - - - - - - - 
+
+# Load packages and set up pathogen -----------------------------------------------
+library(coda)
+library(colorspace)
+library(doMC)
+clust1<-registerDoMC(4)  #change to your number of CPU cores
+library(data.table)
+library(deSolve)
+library(foreach)
+library(ggplot2)
+library(gridExtra)
+library(lubridate)
+library(magrittr)
+library(MASS)
+library(mvtnorm)
+library(tidyverse)
+library(truncnorm)
+
+library(here)
+
+# model run options to change ---------------------------------------------
+seasonal.transmission   <- T # whether to estimate seasonal transmission or not. If false - zeroes BETA_V_AMP after estimation in main script
+include.sero.likelihood <- T
+vector.control          <- T # 
+include.2014.control    <- T # if False then beta_base set to 0
+run.name <- "0219_model1_denv3" 
+
+## MCMC parameters 
+MCMC.runs <- 2000 #number of MCMC iterations 
+thinning.parameter <- 1
+multichain <- c(1:3)  # n chains to run in parallel
+mcmc.burn <- 0.4
+
+# data file names ---------------------------------------------------------
+#locationtab <- c("Central2014","Zika2016") # IF running Central2014 - this needs to go in position no.1
+#dataTab <- c("Central_2014_timeseries","Central_Fiji_2016Z_timeseries") 
+#virusTab <- c("DEN3","Zika")
+locationtab <- c("Zika2016","Central2014") # IF running Central2014 - this needs to go in position no.1
+dataTab <- c("Central_Fiji_2016Z_timeseries","Central_2014_timeseries") 
+virusTab <- c("Zika","DEN3")
+serology.excel <- "Fiji_serology"
+init.conditions.excel <- "thetaR_IC_denv3"
+#iiH <- 1
+locnn <- 2 ## needs to be >1 to set up results matrix not vector
+itertab <- c(1:locnn)
+itertabM=c(1:1) # Iterate over locations in set up and MCMC
+
+# model parameters --------------------------------------------------------
+startdate <- as.Date("2013-01-01")  ## shouldn't be changed (some parameters are relative to this date)
+dt <-  7                            # time step
+start.output.date <- as.Date("2013-01-01")
+end.output.date <- as.Date("2015-11-02")
+seroposdates <- c(as.Date("2013-11-15"), as.Date("2015-11-02"))
+
+# print model run info ------------------------------------------------
+print(paste0("MCMC runs = ", MCMC.runs))
+print(paste0("start date = ", as.Date(startdate)))
+print(paste0("No. serology dates = ", length(seroposdates)))
+print(paste0("Run name = ", run.name))
+print(paste0("No. of chains = ", length(multichain)))
+
+# colours for plots -------------------------------------------------------
+datacol=rgb(0.4,0.4,0.4)
+col2=rgb(0.8,0,0.5,0.8)
+col2a=rgb(0.8,0,0.5,0.4)
+col1=rgb(0,0.3,1,0.8)
+col1a=rgb(0,0.3,1,0.4)
+
+col3=rgb(0.4,0.4,0.4,0.8)
+col3a=rgb(0.4,0.4,0.4,0.4)
+colgrid=rgb(0.4,0.4,0.4,0.1)
+col4=rgb(1,0.7,0.2,0.8)
+col4a=rgb(1,0.7,0.2,0.4)
+
+col5=rgb(1,0.4,0.1,0.8)
+col5a=rgb(1,0.4,0.1,0.4)
+col6=rgb(0.9,0.1,0.1,0.8)
+col6a=rgb(0.9,0.1,0.1,0.4)
+
+col7=rgb(0.02,0.3,0.02,0.8)
+col7a=rgb(0.02,0.3,0.02,0.4)
+
+
+col1 <- "#3880bf"
+col2 <- "#ef286e"
+col3 <- "#c06b74"
+col4 <- "#3eeaef"
+col5 <- "#881448"
+col6 <- "#0ba47e"
+col7 <- "#1e5c4a"
+
+col1a <- alpha("#3880bf" ,0.4)
+col2a <- alpha("#ef286e" ,0.4)
+col3a <- alpha("#c06b74" ,0.4)
+col4a <- alpha("#3eeaef" ,0.4)
+col5a <- alpha("#881448" ,0.4)
+col6a <- alpha("#0ba47e" ,0.4)
+col7a <- alpha("#1e5c4a" ,0.4)
+
+theme_zika_fiji <- function(){
+  theme(title = element_text(size = 12, face = "bold"),
+        panel.background = element_rect(fill = "white",
+                                        colour = "white",
+                                        size = 0.5, linetype = "solid"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.major.y = element_line(size=.1, color=rgb(0,0,0,0.3) ) ,
+        axis.ticks = element_blank(),
+        line = element_line(colour = "gray70"),
+        rect = element_rect(colour = "black", fill = "white")
+  )
+}
+
+
+
+
+
+
+
+
+
+# Fit FP Zika epidemic ----------------------------------------------------
+#zikaFP <- read.csv("data/FP_zika.csv", stringsAsFactors = F)
+#dd <- substr(zikaFP$date,1,2)
+#mm <- substr(zikaFP$date,4,5)
+#Y <- substr(zikaFP$date,7,10)
+#zikaFP$realdate <- paste0(Y,"-",mm,"-",dd)
+#zikaFP$realdate <- as.Date(zikaFP$realdate)
+#
+#zikaFPfull <- data.frame("date"=zikaFP$realdate)
+#full_zika <- with(zikaFP,zikaFP$tahiti + zikaFP$ile_sous + zikaFP$moorea + zikaFP$tuamotu + zikaFP$marquises + zikaFP$australes)
+#avg_rep <- mean(c(zikaFP$tahitiRep,zikaFP$ile_sousRep,zikaFP$mooreaRep,zikaFP$tuamotuRep,zikaFP$marquisesRep,zikaFP$australesRep))
+#
+#full_zika_raw <- full_zika
+#full_zika <- full_zika*(1/avg_rep)
+#
+#zikaFPfull <- cbind(zikaFPfull, "rawZIKV"=full_zika_raw, "ZIKV"=full_zika)
+#zikaFPfull$time <- seq(1,length(zikaFPfull$date)*7,7)
+#
+##zikaFPfull <- zikaFPfull[1:18,]
+#quadraticModel <- lm(zikaFPfull$ZIKV ~ zikaFPfull$time + I(zikaFPfull$time^2))
+#x<-sort(zikaFPfull$time)
+#y<-quadraticModel$fitted.values[order(zikaFPfull$time)]
+#m <- quadraticModel$coefficients[1]
+#beta1 <- quadraticModel$coefficients[2]
+#beta2 <- quadraticModel$coefficients[3]
+#x <- zikaFPfull$time
+#y <- m + beta1*x + beta2*(x^2)
+#
+#offset=0#-(as.numeric(NEW.DATE-as.Date("2013-10-11")))
+#
+#Ctreg <- function(t, offset){
+#  if(offset+t<0){
+#    y2 <- 0
+#    as.numeric(y2)
+#  }else{
+#    tt <- offset+t
+#    y <- m + beta1*tt + beta2*(tt^2)
+#    y2 <- max(y, 0)
+#    as.numeric(y2)#*10) #reporting rate = 10%
+#  }
+#}
+
+# Fit DENV3 epidemic ------------------------------------------------------
+#denv3 <- read.csv("data/Central_2014_timeseries.csv", stringsAsFactors = F)
+#
+#denv3$time <- seq(1,length(denv3$date)*7,7)
+#
+#denv3 <- denv3[,c(1:2,6)]
+#colnames(denv3)[2] <- "DENV3"
+#poissonModel <- glm(denv3$DENV3 ~ denv3$time + I(denv3$time^2), family="poisson")
+#
+#x<-sort(denv3$time)
+#y<-poissonModel$fitted.values[order(denv3$time)]
+#d.m <- poissonModel$coefficients[1]
+#d.beta1 <- poissonModel$coefficients[2]
+#d.beta2 <- poissonModel$coefficients[3]
+#d.x <- denv3$time
+#d.y = exp(d.m + d.beta1*d.x + d.beta2*(d.x^2))
+#
+#D3reg <- function(t){
+#  #y <- exp(d.m + d.beta1*t + d.beta2*(t^2))
+#  y <- exp(1.58971 + 0.1019994*t - 0.0005130412*(t^2))
+#  y2 <- max(y, 0)
+#  as.numeric(y2*(1/0.065)) #reporting rate = 10%
+#}
+#
+#DENVinfections <- as.numeric(y*(1/0.1))
