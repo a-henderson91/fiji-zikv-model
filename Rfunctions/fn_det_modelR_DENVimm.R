5a29e918e863c49dffcdcb9e30e42bac16a7af28
Alasdair Henderson <alasdair.henderson1@lshtm.ac.uk>
2020-11-14 13:20:29 +0000

    Update fn_det_modelR_DENVimm.R

--- Rfunctions/fn_det_modelR_DENVimm.R
+++ Rfunctions/fn_det_modelR_DENVimm.R
@@ -9,8 +9,8 @@
 #' @param include.count True or False - whether to include count data in likelihood. Defaults to True
 #' @keywords deterministic
 #' @export
-#theta=c(theta_star,thetaA_star,theta_denv); theta_init=theta_init_star; locationI=locationtab[iiH]; seroposdates=seroposdates; include.count=include.count
-Deterministic_modelR_final_DENVimmmunity <- function(theta, theta_init, locationI, seroposdates, episeason, include.count=T){
+#theta = c(theta_star,thetaA_star,theta_denv); theta_init = theta_init_star; seroposdates=seroposdates
+Deterministic_modelR_final_DENVimmmunity <- function(theta, theta_init, locationI = "Zika2016", seroposdates, episeason, include.count=T){
     theta[["denv_start_point"]] <- as.Date("2013-10-27") - startdate 
     theta[["zika_start_point"]] <- theta[["intro_mid"]] ## these are poorly named. "zika_start_point" just refers to the main disease of interest. And 'denv_start_point' is the fixed background DENV3
 
@@ -57,25 +57,16 @@ Deterministic_modelR_final_DENVimmmunity <- function(theta, theta_init, location
 
     # Calculate seropositivity at pre-specified dates and corresponding likelihood
     i=1; seroP=NULL; binom.lik=NULL
-    sero.years <- format(as.Date(seroposdates, format="%d/%m/%Y"),"%Y")
-    sero.y <- substr(sero.years,3,4)
-    lum.y <- c("13","15","17")
     if(include.sero.likelihood==T){
-    for(date in seroposdates){
-      if(date < min(date.vals)){ # if seroprevalence date is before Zika timeline { seroprevalence = Luminex data + epislon}
-        seroP[i] <- epsilon
-        binom.lik[i] <- (dbinom(nLUM[lum.y==sero.y[i]], size=nPOP[lum.y==sero.y[i]], prob=seroP[i], log = T))
-      }else{ # else { seroprevalence = model predicted recovered as a proportion of pop + epsilon }
-          seroP[i] <-  (min(R_traj[date.vals<date+3.5 & date.vals>date-3.5])/theta[["npop"]]) +
-                        (1 - min(R_traj[date.vals<date+3.5 & date.vals>date-3.5])/theta[["npop"]])*epsilon
-          binom.lik[i] <- (dbinom(nLUM[lum.y==sero.y[i]], size=nPOP[lum.y==sero.y[i]], prob=seroP[i], log = T))
-          }
+      for(date in seroposdates){
+          seroP[i] <-  (min(R_traj[date.vals<date+3.5 & date.vals>date-3.5])/theta[["npop"]]) + 
+            (1 - min(R_traj[date.vals<date+3.5 & date.vals>date-3.5])/theta[["npop"]])*epsilon
+          binom.lik[i] <- (dbinom(nLUM[i], size=nPOP[i], prob=seroP[i], log = T))
         i <- i+1
-      }
-    }else{
-      binom.lik=0
-    }
-    date <- seroposdates[1]
+        }
+      }else{
+        binom.lik=0
+        }
     ln.denv <- length(denv.timeseries)
     ln.full <- length(y.vals)
     first.zikv <- min(which(y.vals>0))
